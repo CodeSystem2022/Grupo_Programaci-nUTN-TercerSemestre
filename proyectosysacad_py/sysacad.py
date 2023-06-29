9c401fc8743c093492bdbad9d83816b47fef95e4
NahuelAlaniz <104447527+NahuelAlaniz@users.noreply.github.com>
2023-06-29 12:07:18 -0300

    Update sysacad.py
    Cogido de botones

--- proyectosysacad_py/sysacad.py
+++ proyectosysacad_py/sysacad.py
@@ -19,3 +19,28 @@ class Bienvenida(tk.Toplevel):
 
     def cerrar_ventana(self):
         self.destroy()
+
+
+   def mostrar_opcion1(self):
+        self.withdraw()  # Ocultar la ventana del menú
+        opcion1_window = Opcion1Window(self)  # Crear una nueva ventana para la opción 1
+
+    def mostrar_opcion2(self):
+        self.withdraw()
+        opcion2_window = Opcion2Window(self)  # Crear una nueva ventana para la opción 2
+
+    def mostrar_opcion3(self):
+        self.withdraw()
+        opcion3_window = Opcion3Window(self)  # Crear una nueva ventana para la opción 3
+
+    def mostrar_opcion4(self):
+        self.withdraw()
+        opcion3_window = Opcion4Window(self)  # Crear una nueva ventana para la opción 4
+
+    def mostrar_opcion5(self):
+        self.withdraw()
+        opcion3_window = Opcion5Window(self)  # Crear una nueva ventana para la opción 5
+
+    def mostrar_opcion6(self):
+        self.destroy()
+        self.master.deiconify()
